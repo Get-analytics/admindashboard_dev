c1849a2246ab640cdf33c276e5ba814d86d560cc
Get-analytics <team20tech@gmail.com>
2025-03-25 21:21:55 +0530

    Update Linkpage.js
    url updated

--- src/pages/Linkpage.js
+++ src/pages/Linkpage.js
@@ -66,7 +66,7 @@ const Linkpage = () => {
   const fetchDashboardData = async (authToken, UUID) => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/v1/client/dashboard",
+        "https://admin-dashboard-backend-rust.vercel.app/api/v1/client/dashboard",
         { authToken, UUID }
       );
       if (response.status === 200) {
